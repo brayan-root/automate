aba47a5efcbd2a78c533f5e033da6ae4d1610003
Brian Root <mwangi.ict@gmail.com>
2022-01-19 14:24:25 +0300

    Using For Loop to Iterate Through a file

--- readingwithkeyword.py
+++ readingwithkeyword.py
@@ -2,3 +2,5 @@
 #Right on! Both methods read from the current position. The readline() method reads one line, while read() reads until the end of the file.
 with open("new.txt") as file:    #python automatically closes the file 
     print(file.readline()) 
+    
+#Using a "with" block is a good way to open and work on a single file then have the file automatically closed at the end of the block. On the flip side, using open outside of a block means we can use a file object in other places in our code. So we're not restricted to just one single block.
